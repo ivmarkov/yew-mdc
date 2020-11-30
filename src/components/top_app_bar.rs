6aeb202be55192a22701916fd171709c1c2c5a08
Ivan Markov <ivan.markov@gmail.com>
2020-11-30 21:11:00 +0200

    Support for fixed top app bar

--- src/components/top_app_bar.rs
+++ src/components/top_app_bar.rs
@@ -15,6 +15,8 @@ pub struct Props {
     #[prop_or_default]
     pub manualrows: bool,
     #[prop_or_default]
+    pub fixed: bool,
+    #[prop_or_default]
     pub classes: String,
 }
 
@@ -40,20 +42,32 @@ impl Component for TopAppBar {
     }
 
     fn view(&self) -> Html {
-        let classes = format!("mdc-top-app-bar {}", self.props.classes);
+        let classes = format!("mdc-top-app-bar {} {}", self.props.classes, if self.props.fixed {"mdc-top-app-bar--fixed"} else {""});
+        let html;
         if self.props.manualrows {
-            html! {
+            html = html! {
                 <header class=classes id=&self.props.id>
                     { self.props.children.clone() }
                 </header>
             }
         } else {
-            html! {
+            html = html! {
                 <header class=classes id=&self.props.id>
                     <div class="mdc-top-app-bar__row">
                         { self.props.children.clone() }
                     </div>
                 </header>
+            }
+        }
+
+        if !self.props.fixed {
+            html
+        } else {
+            html! {
+                <>
+                    { html }
+                    <div class="mdc-top-app-bar--fixed-adjust"/>
+                </>
             }
         }
     }
